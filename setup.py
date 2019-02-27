3328f7972e74404d816b5bd0575e70bbb3525bfd
Friedger Müffke <mail@friedger.de>
2019-02-27 16:48:57 +0100

    version 0.6.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matrix_blockstack_password_provider",
-    version="0.6.2",
+    version="0.6.3",
     author="OpenIntents",
     author_email="support@openintents.org",
     description="Password provider for blockstack/EOS synapse server",
