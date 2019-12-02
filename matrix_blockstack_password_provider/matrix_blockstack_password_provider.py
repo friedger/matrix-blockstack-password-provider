f685f508bc5565a2bce2a2429098e4111f895227
Friedger Müffke <mail@friedger.de>
2019-12-02 20:24:44 +0100

    add call to sign token

--- matrix_blockstack_password_provider/matrix_blockstack_password_provider.py
+++ matrix_blockstack_password_provider/matrix_blockstack_password_provider.py
@@ -116,9 +116,11 @@ class BlockstackPasswordProvider(object):
         logger.info(pubKey)
         sig = bytes.fromhex(crypto_segment.hex())
         logger.info(sig)
+        privateKey = coincurve.PrivateKey.from_hex("278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f")
         logger.info("--")
+        jwt = privateKey.sign("{\"issuedAt\":\"1440713414.85\",\"challenge\":\"7cd9ed5e-bb0e-49ea-a323-f28bde3a0549\",\"issuer\":{\"publicKey\":\"03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479\",\"chainPath\":\"bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39\",\"publicKeychain\":\"xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ\",\"blockchainid\":\"ryan\"}}".encode("utf-8"))
+        logger.info(base64.urlsafe_b64encode(jwt))
         coincurve.verify_signature(sig, payload_segment,  pubKey)
-
         decodedToken = yield jwt.decode(password,publicKey, False, ["ES256K"])
         now = (datetime.now(timezone.utc)-datetime(1970,1,1,tzinfo=timezone.utc)).total_seconds()
         logger.info("%d %d %d", now, decodedToken["iat"], decodedToken["exp"])
